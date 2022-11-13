fa5653c27cd74f91de490a1fbae10add88417963
Sultan Rushud <Sultankh1212@hotmail.com>
2022-11-13 03:29:55 +0300

    setupAssignment
    finished setup assignment

--- scripts/13_setup_machine.sh
+++ scripts/13_setup_machine.sh
@@ -3,11 +3,14 @@
 
 function installPackages(){
 # TODO: Install needed libries
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install git
 }
 
 function createShortcuts(){
 # TODO: Create aliases and add them to your shell profile (~/.zshrc or ~/.bashrc)
+echo 'alias today="echo This is a `date +"%A %d in %B of %Y "`"' >> ~/.bashrc
+echo 'alias cpui="cat /proc/cpuinfo"' >> ~/.bashrc
 }
-
-
-
+installPackages
+createShortcuts
